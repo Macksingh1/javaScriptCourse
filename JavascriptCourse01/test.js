45aab432f4234c1864bfc95ea464d0e121349d4f
Macksingh1 <109999843+Macksingh1@users.noreply.github.com>
2023-12-14 22:08:21 +0000

    function basics

--- JavascriptCourse01/test.js
+++ JavascriptCourse01/test.js
@@ -1,3 +1,18 @@
 console.log("Hello");
 // for loop 
 
+const score = 10098837262738939n
+console.log(typeof score)
+
+const id = Symbol(score)
+console.log(score)
+
+const score1 = 100
+
+console.log(score === score1)
+
+const testFunc = function () {
+    console.log("Print from function");
+}
+
+testFunc()
