bbaf1a5e93ace69b77459820415bc7269071ac0d
Macksingh1 <109999843+Macksingh1@users.noreply.github.com>
2023-12-19 21:52:32 +0000

    TEST 01 FILE ADD

--- JavascriptCourse01/test01.js
+++ JavascriptCourse01/test01.js
@@ -1,18 +1,44 @@
-let data = "names"
+// let data = "  names. "
 
-let userOne = {
-    name: "Jack",
-    phone: 10101
+// let userOne = {
+//     name: "Jack",
+//     phone: 10101
+// }
+
+// let userTwo = {
+//     name: "John",
+//     phone: 45639
+// }
+
+// console.log(userOne)
+// userOne = userTwo
+// userTwo.phone = 9999
+// console.log(userOne)
+
+// console.log(data);
+// console.log(data.trim());
+
+// console.log(Math.round(4.4))
+
+const month = 3
+
+switch (month) {
+    case 1:
+        console.log("January")
+        break;
+    case 2:
+        console.log("Feburary")
+        break;
+    case 3:
+        console.log("March")
+        break;
+
+    default:
+        console.log("Nothing");
+        break;
 }
 
-let userTwo = {
-    name: "John",
-    phone: 45639
-}
 
-console.log(userOne)
-userOne = userTwo
-userTwo.phone = 9999
-console.log(userOne)
+let price = 600
+price <500 ? console.log("Less") : console.log("more");
 
-console.log(data);
